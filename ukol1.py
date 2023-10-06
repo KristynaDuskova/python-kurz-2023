ff4f45703968d857ad2c2b82bfcd87a656161f51
Kristyna Duskova <Tyna.Duskova20@seznam.cz>
2023-10-06 13:27:08 +0200

    commit

---
+++ ukol1.py
@@ -0,0 +1,14 @@
+jmeno = (input("Vypište své jméno: "))
+print(jmeno + "@czechitas.cz")
+
+
+#Bonus1
+jmeno_a_prijmeni = (input("tvé jméno: "))
+print(jmeno_a_prijmeni.upper())
+
+#Bonus2
+jmeno = input("Vypiš své jméno: ")
+prijmeni = input("Zadej příjmení: ")
+inicialy = jmeno[0] + prijmeni[0]
+print("Iniciály:", inicialy.upper())
+
