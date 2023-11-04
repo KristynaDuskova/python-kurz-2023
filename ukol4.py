55f289acbe459c8ac46d3235bc253efff2050430
KristynaDuskova <146025265+KristynaDuskova@users.noreply.github.com>
2023-11-04 20:37:20 +0100

    ukol4.py

--- ukol4.py
+++ ukol4.py
@@ -9,5 +9,36 @@
 #Tvůj program nejprve ověří pomocí první funkce správnost telefonního čísla. Pokud není platné, vypíše chybu, v opačném případě se zeptá na text zprávy a pomocí druhé funkce určí její cenu, kterou vypíše uživateli
 
 
-program = ("Zadejte své číslo: ")
-sms = ("Napište zprávu: ")
+import math
+
+def is_correct(number):
+    digits = len(number)
+
+    if digits == 9:
+        return True
+    elif digits == 13:
+        if number[0:4] == "+420":
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def calculate(sms):
+    count = len(sms)
+
+    return 3 * math.ceil(count / 180)
+
+
+program = input("Zadejte své číslo: ")
+
+if is_correct(program):
+    print("Vaše číslo je v pořádku.")
+else:
+    print("Číslo je špatně zapsané!")
+    exit()
+
+sms = input("Napište zprávu: ")
+price = calculate(sms)
+
+print("Cena:", price, "kč")
